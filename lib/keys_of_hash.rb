679e2397fe3dae0877f7f4d21fdf2f7ce6e83d81
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-12 17:36:03 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,24 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    arr = []
+    map.each do |key, val|
+      if arguments.include?(val)
+        arr << key
+      end
+    end
+    return arr
   end
 end
+
+=begin
+test_hash = {:animals  =>
+  { "sugar glider"=>"Australia",
+    "aye-aye"=> "Madagascar",
+    "red-footed tortoise"=>"Panama",
+    "kangaroo"=> "Australia",
+    "tomato frog"=>"Madagascar",
+    "koala"=>"Australia"} 
+}
+=end
+#puts test_hash.is_a?(Hash)
+#puts test_hash.keys_of('Panama')
