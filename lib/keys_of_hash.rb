b852b5a5d927fdc8d4a4bc64593fdc99f3cb44af
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-12 16:48:42 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -6,3 +6,14 @@ class Hash
     return arr
   end
 end
+
+test_hash =
+:animals  => { 
+  { "sugar glider"=>"Australia",
+    "aye-aye"=> "Madagascar",
+    "red-footed tortoise"=>"Panama",
+    "kangaroo"=> "Australia",
+    "tomato frog"=>"Madagascar",
+    "koala"=>"Australia"} 
+}
+
